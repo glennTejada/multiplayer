37bf2b60370df358aced1876c2602f675335e671
unknown <glenntejada2021@mailfence.com>
2022-03-04 07:22:43 -0800

    Changes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path")
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server,{ cors: { origin: "*"}})
+let players= [];
+
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs");
@@ -21,7 +23,33 @@ server.listen(8000, () => {
 io.on('connection', (socket) => {
     console.log("User Id: "+socket.id)
 
+    if(!players.includes(socket.id)){
+
+    console.log("NP");
+        // send a message to the client
+    socket.send(JSON.stringify({
+        type: "new_player",
+        content: {"id":socket.id,"players":players}
+    }));
+    socket.broadcast.emit('message', (JSON.stringify({
+        type: "joined",
+        content: {"id":socket.id}
+    })))
+    players.push(socket.id);
+        
+
+    }
+
+    socket.on('disconnect', () => {
+        console.log(socket.id+'user disconnected');
+      });
+
+
     socket.on('message', (data) => {
-        socket.broadcast.emit('message', (data))
+        
+        var data = JSON.parse(data);
+        data.content = socket.id+","+data.content;
+        console.log(data);
+        socket.broadcast.emit('message', JSON.stringify(data))
     })
 })
--- public/assets/VolcanicRegionMapDemo.js
+++ public/assets/VolcanicRegionMapDemo.js
@@ -1,8 +1,10 @@
-﻿class VolcanicRegionMapDemo extends Phaser.Scene {
+﻿
+class VolcanicRegionMapDemo extends Phaser.Scene {
     constructor() {
         super();
-    }
+        this.players = {};
 
+    }
     preload() {
         this.load.image('bg', '/assets/VolcanicRegion.png');
 
@@ -23,20 +25,66 @@
     }
 
     update() {
+ 
         this.player.setVelocity(0);
 
+
+        if(the_data.type=="new_player"){
+            myID = the_data.content.id;
+            for(var i=0; i<the_data.content.players.length; i++){
+         
+                this.players[the_data.content.players[i]] = this.physics.add.image(400, 300, 'block');
+            
+            }
+    
+           the_data.type = "";
+        }
+
+        if(the_data.type=="joined"){
+         
+
+               this.players[the_data.content.id] = this.physics.add.image(400, 300, 'block');
+            
+           the_data.type = "";
+        }
+
+
+
+
+        if(the_data.type=="move"){
+            var values = the_data.content.split(',');
+     console.log(this.players);
+            try{
+                
+            this.players[values[0]].x = parseInt(values[1]);
+            this.players[values[0]].y = parseInt(values[2]);
+            
+        }
+        catch(err){
+
+        }the_data.type = "";
+    
+    }
+      
+
+   
         if (this.cursors.left.isDown) {
+           
             this.player.setVelocityX(-200);
+            send_now(this.player.x+","+this.player.y);
         }
         else if (this.cursors.right.isDown) {
             this.player.setVelocityX(200);
+            send_now(this.player.x+","+this.player.y);
         }
 
         if (this.cursors.up.isDown) {
             this.player.setVelocityY(-200);
+            send_now(this.player.x+","+this.player.y);
         }
         else if (this.cursors.down.isDown) {
             this.player.setVelocityY(200);
+            send_now(this.player.x+","+this.player.y);
         }
     }
 }
